387adb609f189a2f07175cb23a064cd3b0e2ce45
acomesanavila <acomesanavila@victoria7.danielcastelao.org>
2016-10-25 08:36:40 +0200

    branche

---
+++ boletin5_2/src/boletin5_2/Boletin5_2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin5_2;
+
+/**
+ *
+ * @author acomesanavila
+ */
+public class Boletin5_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Numero numero= new Numero();
+        numero.cuentas((short)2,(short)3);
+    }
+    
+}
---
+++ boletin5_2/src/boletin5_2/Numero.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin5_2;
+
+/**
+ *
+ * @author acomesanavila
+ */
+public class Numero {
+    private short num1,num2;
+    public Numero (){
+        num1=0;
+        num2=0;
+    }
+    public Numero(short n1, short n2){
+        num1=n1;
+        num2=n2;               
+    }
+    public void cuentas (short n1,short n2){
+        /*if (n1>n2)
+            System.out.println("resta =" + (n1-n2)); 
+        else
+            System.out.println("suma = "+ (n1+n2));*/
+        short numero=(short) ((n1>n2)? (n1-n2):(n1+n2));
+        System.out.println("resultado ="+ numero);
+    }        
+    
+}
