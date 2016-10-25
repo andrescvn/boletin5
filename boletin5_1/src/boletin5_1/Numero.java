7387529b6c658240fef28bae06f64d84431bd75b
acomesanavila <acomesanavila@victoria7.danielcastelao.org>
2016-10-25 08:35:37 +0200

    branche

---
+++ boletin5_1/src/boletin5_1/Numero.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin5_1;
+
+/**
+ *
+ * @author acomesanavila
+ */
+public class Numero {
+    private float num1;
+    public Numero (){
+        
+    }
+    public Numero (float num1){
+    this.num1=num1;
+    }
+    public void positivo (float n1) {
+    if (n1>0)
+            System.out.println("E positivo");
+    }
+    
+}
